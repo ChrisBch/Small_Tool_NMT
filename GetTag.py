2e1872539c10b4719b3c1dc82efd8e333495cd29
ChrisYuan <32697489+ChrisBch@users.noreply.github.com>
2018-07-10 14:06:58 +0800

    Update GetTag.py

--- GetTag.py
+++ GetTag.py
@@ -3,14 +3,23 @@
 from nltk.tag import StanfordPOSTagger
 import os
 
-os.environ["JAVA_HOME"] = "C:\Program Files\Java\jre1.8.0_171" 
-os.environ["CLASSPATH"] = "D:\StanfordNLP\jars"
-os.environ["STANFORD_MODELS"] = "D:\StanfordNLP\models"
+#you can chose your own environment path 
+#e.g. os.environ["JAVA_HOME"] = "C:\Program Files\Java\jre1.8.0_171" 
+#os.environ["CLASSPATH"] = "D:\StanfordNLP\jars"
+#os.environ["STANFORD_MODELS"] = "D:\StanfordNLP\models"
+
+os.environ["JAVA_HOME"] = "path to java" 
+os.environ["CLASSPATH"] = "path to StanfordTools\jars"
+os.environ["STANFORD_MODELS"] = "path to StanfordTools\models"
 
 eng_tagger = StanfordPOSTagger('english-bidirectional-distsim.tagger')
 chn_tagger = StanfordPOSTagger('chinese-distsim.tagger')
 
-with open (r"F:\AllTag\chn_eng_train.zh.txt","r",encoding="utf-8_sig") as ChnReader,open (r"F:\AllTag\chn_eng_train.en.txt","r",encoding="utf-8_sig") as EngReader:
+#enter the path to your corpus that you want to tag
+#e.g. r"F:\AllTag\chn_eng_train.zh.txt"
+#r"F:\AllTag\chn_eng_train.en.txt"
+
+with open (r"path to chinese corpus","r",encoding="utf-8_sig") as ChnReader,open (r"path to english corpus","r",encoding="utf-8_sig") as EngReader:
     ChnTag = []
     EngTag = []
     for index,line in enumerate(ChnReader):
